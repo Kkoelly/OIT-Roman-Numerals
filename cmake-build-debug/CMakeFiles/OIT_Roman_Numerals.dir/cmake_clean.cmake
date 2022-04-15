d714fbcd6f3a7811cfd20aed909d1dbba7b5eb85
Kkoelly <kellyko2003@gmail.com>
2022-04-14 18:28:26 -0600

    initial commit

---
+++ cmake-build-debug/CMakeFiles/OIT_Roman_Numerals.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OIT_Roman_Numerals.dir/main.cpp.obj"
+  "OIT_Roman_Numerals.exe"
+  "OIT_Roman_Numerals.exe.manifest"
+  "OIT_Roman_Numerals.pdb"
+  "libOIT_Roman_Numerals.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OIT_Roman_Numerals.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/OIT_Roman_Numerals.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for OIT_Roman_Numerals.
+# This may be replaced when dependencies are built.
