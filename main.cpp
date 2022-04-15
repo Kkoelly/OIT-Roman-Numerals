b678840479ec4ddf04fa26bfb29a866fd0a65f56
Kkoelly <kellyko2003@gmail.com>
2022-04-14 18:53:54 -0600

    initial solution

--- main.cpp
+++ main.cpp
@@ -1,6 +1,44 @@
 #include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    map<char, int> romanNums = {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000},
+
+    };
+    string input = "CDXXX";
+    int sum = 0;
+
+    for (int i = 0; i < input.length(); i++) {
+        if (i == input.length() - 1) {
+            sum += romanNums[input[i]];
+            break;
+        }
+        if (romanNums[input[i+1]] <= romanNums[input[i]]) {
+            sum += romanNums[input[i]];
+        }
+        else {
+            if (romanNums[input[i]] * 10 < romanNums[input[i+1]]) {
+                cout << "NOT VALID INPUT" << endl;
+                break;
+            }
+            else {
+                sum -= romanNums[input[i]];
+            }
+        }
+    }
+
+    cout << sum << endl;
+    
+
     return 0;
 }
